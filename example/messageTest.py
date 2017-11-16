b475027954ac01d015028d755ba35dbe4fce0bb3
杨福照 <yangfuzhao@ipaynow.cn>
2017-11-16 15:15:20 +0800

    1

--- example/messageTest.py
+++ example/messageTest.py
@@ -3,15 +3,15 @@
 from ipaynowIdentityPythonSdk.ipaynow_identity.authService import toCheckID, queryCheckID, toCheckCard, queryCheckCard, \
     toCheckMobileNo, queryCheckMobileNo
 
-# print(toCheckID("1441071499740581","DK96gnOB7EmVDDaHgLTLEZqVgP0H0nML","abnMX6YXHjBdesCsn2TD8b25","checkId1","522224199102072416","杨福照"))
+# print(toCheckID("xxxxxxxxx","DK96gnOB7EmVDDaHgLTLEZqVgP0H0nML","abnMX6YXHjBdesCsn2TD8b25","checkId1","xxxxxxxxxxxxxxxxxxxx","xxx"))
 
 #身份认证查询
-# print(queryCheckID("1441071499740581","DK96gnOB7EmVDDaHgLTLEZqVgP0H0nML","abnMX6YXHjBdesCsn2TD8b25","checkId1"))
+# print(queryCheckID("xxxxxxxxxxxxxxxx","xxxxxxxxxxxxxxxxx","xxxxxxxxxxxxxxxxx","checkId1"))
 
-# print(toCheckCard("1441071499740581","DK96gnOB7EmVDDaHgLTLEZqVgP0H0nML","Z5CbXbEaJA6CYRSDS5Ddsab6","pythonCardbeijin3","522222222","xxx","01","dddddd","xxxxx"))
+# print(toCheckCard("xxxxxxxxxxxxxxxxxxxx","xxxxxxxxxxxxxxxx","xxxxxxxxxxxxxxxxxxxx","pythonCardbeijin3","xxxxxxxxxxxxxxxxxxxxxxxxx","xxx","01","dddddd","xxxxx"))
 
-# print(queryCheckCard("1441071499740581","DK96gnOB7EmVDDaHgLTLEZqVgP0H0nML","Z5CbXbEaJA6CYRSDS5Ddsab6","pythonCardbeijin0"))
+# print(queryCheckCard("xxxxxxxxxxxxxxxxxxxxx","xxxxxxxxxxxxxxxxxxx","xxxxxxxxxxxxxxxxx","pythonCardbeijin0"))
 
-# print(toCheckMobileNo("1476788813528909","c4MlX9GJCi0YI5z3RvpK17wPlscFKpY1","dYTAwfNMEeT3pk5FB8MS5MYk","pythonCardbeijin3","522222222","xxx","01","xxxxxxxxxxxxx"))
+# print(toCheckMobileNo("xxxxxxxxxxxxxxxxxxx","xxxxxxxxxxxxxxxxxxxxxx","xxxxxxxxxxxxxxxxxxxxxx","pythonCardbeijin3","xxxxxxxxxxxxxxxxxxxxxxxx","xxx","01","xxxxxxxxxxxxx"))
 
-# print(queryCheckMobileNo("1476788813528909","c4MlX9GJCi0YI5z3RvpK17wPlscFKpY1","dYTAwfNMEeT3pk5FB8MS5MYk","pythonCardbeijin3"))
+# print(queryCheckMobileNo("xxxxxxxxxxxxxxxxxxxx","xxxxxxxxxxxxxxxxxxx","xxxxxxxxxxxxxxxxxxx","pythonCardbeijin3"))
